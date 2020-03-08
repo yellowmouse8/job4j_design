74a146f6d129632e676edc3fc34715f67f302d80
Pavel <yellowmouse8@protonmail.com>
2020-03-09 00:03:32 +0300

    Принципы Kiss, Dry и Yagni

--- TDD/src/main/java/ru/design/principe/Search.java
+++ TDD/src/main/java/ru/design/principe/Search.java
@@ -3,6 +3,7 @@ package ru.design.principe;
 import javax.xml.namespace.QName;
 import javax.xml.stream.XMLInputFactory;
 import java.io.File;
+import java.security.cert.CertificateParsingException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -12,30 +13,27 @@ import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class Search {
-    public Integer max(List<Integer> value) {
-        int max = value.get(0);
-        for (Integer i : value) {
-            if (i > max) {
-                max = i;
-            }
-        }
-        return max;
+    public Integer max(List<Integer> value, Comparator<Integer> comparator) {
+        return Collections.max(value, comparator);
     }
 
-    public Integer min(List<Integer> value) {
-        int min = value.get(0);
-        for (Integer val : value) {
-            if (val < min) {
-                min = val;
-            }
-        }
-        return min;
+    public Integer min(List<Integer> value, Comparator<Integer> comparator) {
+        return Collections.min(value, comparator);
     }
 
     public static void main(String[] args) {
         List<Integer> list = List.of(2, 3, 4, 5, 6, 7, 54, 34);
         Search search = new Search();
-        System.out.println(search.max(list));
-        System.out.println(search.min(list));
+        System.out.println(search.max(list, Comparator.naturalOrder()));
+                /* Или можно написать вот так ->  new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o1.compareTo(o2);
+            }
+        }));
+
+                 */
+
+        System.out.println(search.min(list, Comparator.naturalOrder()));
     }
 }
