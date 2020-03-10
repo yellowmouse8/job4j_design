32caa02bdffbd455862c25743b8d6a89c469edda
Pavel <yellowmouse8@protonmail.com>
2020-03-10 21:05:28 +0300

    Принципы Kiss, Dry и Yagni

--- TDD/src/main/java/ru/design/principe/Search.java
+++ TDD/src/main/java/ru/design/principe/Search.java
@@ -1,5 +1,7 @@
 package ru.design.principe;
 
+import javax.print.DocFlavor;
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
 import javax.xml.namespace.QName;
 import javax.xml.stream.XMLInputFactory;
 import java.io.File;
@@ -14,7 +16,13 @@ import java.util.stream.Collectors;
 
 public class Search {
     public Integer max(List<Integer> value, Comparator<Integer> comparator) {
-        return Collections.max(value, comparator);
+        int max = value.get(0);
+        for (Integer val : value) {
+            if (val > max) {
+                max = val;
+            }
+        }
+        return max;
     }
 
     public Integer min(List<Integer> value, Comparator<Integer> comparator) {
@@ -25,15 +33,6 @@ public class Search {
         List<Integer> list = List.of(2, 3, 4, 5, 6, 7, 54, 34);
         Search search = new Search();
         System.out.println(search.max(list, Comparator.naturalOrder()));
-                /* Или можно написать вот так ->  new Comparator<Integer>() {
-            @Override
-            public int compare(Integer o1, Integer o2) {
-                return o1.compareTo(o2);
-            }
-        }));
-
-                 */
-
         System.out.println(search.min(list, Comparator.naturalOrder()));
     }
 }
