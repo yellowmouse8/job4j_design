c321dbce7af75960758360a7a301df0a36e60169
Pavel <yellowmouse8@protonmail.com>
2020-03-14 22:25:43 +0300

    SimpleGenerator

--- TDD/src/main/java/ru/design/principe/SimpleGenerator.java
+++ TDD/src/main/java/ru/design/principe/SimpleGenerator.java
@@ -5,25 +5,19 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class SimpleGenerator implements Template {
-    private final String STROKE = "^\\$\\{[a-z]+}$";
+    private final String STROKE = "^\\$\\W+[A-Za-z]+\\W+$";
     private final Pattern PATTERN = Pattern.compile(STROKE);
 
     @Override
     public String generate(String template, Map<String, String> stroke) {
         Matcher m = PATTERN.matcher(template);
         StringBuffer sb = new StringBuffer();
-        int i = 0;
-        while (m.find()) {
-            String group = m.group();
-            String replacement;
-            if (stroke.containsKey(group)) {
-                replacement = stroke.get(group);
+        while (m.find()){
+            if (stroke.containsKey(m.group(1))){
+                m.appendReplacement(sb, stroke.get(m.group(1)));
             } else {
-                i++;
-                replacement = "^[a-zA-Z]+\\W+";
-                stroke.put(group, replacement);
+                m.appendReplacement(sb, m.group(0));
             }
-            m.appendReplacement(sb, replacement);
         }
         m.appendTail(sb);
         return sb.toString();
