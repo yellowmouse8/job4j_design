0d25f3beaa976507598ac3b0f5b94e29118b3164
Pavel <yellowmouse8@protonmail.com>
2020-03-16 22:38:50 +0300

    SimpleGenerator

--- TDD/src/main/java/ru/design/principe/SimpleGenerator.java
+++ TDD/src/main/java/ru/design/principe/SimpleGenerator.java
@@ -2,15 +2,26 @@ package ru.design.principe;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class SimpleGenerator implements Template {
+    private final String STROKE = "\\$\\{[A-Za-z]+\\W";
+    private final Pattern PATTERN = Pattern.compile(STROKE);
 
     @Override
     public String generate(String template, Map<String, String> stroke) {
-        return stroke.keySet().stream().reduce(template, (value, key) -> value
-                .replaceAll("\\$\\{" + key + "\\W", stroke.get(key)));
-
+        Matcher m = PATTERN.matcher(template);
+        StringBuffer sb = new StringBuffer();
+        while (m.find()) {
+            if (stroke.containsKey(m.group())) {
+                m.appendReplacement(sb, stroke.get(m.group()));
+            } else {
+                throw new IllegalStateException("Не соответствие шаблону ! ");
+            }
+        }
+        m.appendTail(sb);
+        return sb.toString();
     }
 }
--- TDD/src/test/java/ru/design/principe/TemplateTest.java
+++ TDD/src/test/java/ru/design/principe/TemplateTest.java
@@ -17,7 +17,7 @@ public class TemplateTest {
         //assign
         Template template = new SimpleGenerator();
         String text = "I am ${name}, who are ${subject}?";
-        Map<String, String> data = Map.of("name", "Pi", "subject", "you");
+        Map<String, String> data = Map.of("${name}", "Pi", "${subject}", "you");
         String checked = "I am Pi, who are you?";
         //act
         String result = template.generate(text, data);
@@ -27,14 +27,20 @@ public class TemplateTest {
     }
 
     @Test
-    public void whenTakeThreeNames() {
+    public void whenAllSubjectsDone() {
         Template template = new SimpleGenerator();
-        String text = "${name}, ${sos}, ${sos}, ${sos}!";
-        Map<String, String> data = Map.of("name", "Help", "sos", "Aaaa");
-        String checked = "Help, Aaaa, Aaaa, Aaaa!";
+        String text = "${name} , ${sos} , ${sos} , ${sos} !";
+        Map<String, String> data = Map.of("${name}", "Help", "${sos}", "Aaaa");
+        String checked = "Help , Aaaa , Aaaa , Aaaa !";
         String result = template.generate(text, data);
         Assert.assertThat(result, is(checked));
-
     }
-
+    @Test (expected = IllegalStateException.class)
+    public void whenAllSubjectsIncorrect() {
+        Template template = new SimpleGenerator();
+        String text = "${name} , ${sos} , ${sos} , ${sos} !";
+        Map<String, String> data = Map.of("${name}", "Help","${sss}", "Aaaa");
+        String result = template.generate(text, data);
+        System.out.println(result);
+    }
 }
