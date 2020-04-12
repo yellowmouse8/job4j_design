e5d018b6986db3af20a96ac322b3d8d77c85f8b5
Pavel <yellowmouse8@protonmail.com>
2020-04-12 23:50:22 +0300

    Сканирование файлов DirectoryStream

--- IO/src/main/java/ru/job4j/io/Search.java
+++ IO/src/main/java/ru/job4j/io/Search.java
@@ -7,23 +7,23 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Search {
     public static void main(String[] args) throws IOException {
         Path start = Paths.get(".");
-        search(start, "txt");
+        System.out.println(search(start, "txt"));
     }
 
     public static List<String> search(Path root, String ext) throws IOException {
         List<String> list = new ArrayList<>();
         try
                 (DirectoryStream<Path> stream = Files.newDirectoryStream(root)) {
+
             for (Path path : stream) {
                 if (!Files.isDirectory(path)) {
-                    if (path.endsWith(ext)) {
+                    if (path.getFileName().toString().endsWith(ext)) {
                         list.add(path.getFileName().toString());
-                    } else {
-                        System.out.println(" File not found. ");
                     }
                 }
             }
