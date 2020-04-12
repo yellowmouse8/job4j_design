ea516f03a7a6a257b89b15a0e0953e77b5944377
Pavel <yellowmouse8@protonmail.com>
2020-04-12 22:57:31 +0300

    Сканирование файловой системы

--- IO/src/main/java/ru/job4j/io/Search.java
+++ IO/src/main/java/ru/job4j/io/Search.java
@@ -1,32 +1,31 @@
 package ru.job4j.io;
 
-import java.awt.*;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
-
-import static java.nio.file.Files.*;
 
 public class Search {
     public static void main(String[] args) throws IOException {
         Path start = Paths.get(".");
-        search(start, "xml");
+        search(start, "txt");
     }
-
 
     public static List<String> search(Path root, String ext) throws IOException {
         List<String> list = new ArrayList<>();
-        List<Path> directories = List.of(root.toAbsolutePath());
-        for (Path path : directories) {
-            if (path.getFileName().endsWith(ext)) {
-                list.add(path.getFileName().toString());
-            } else {
-                System.out.println(" Files not found! ");
+        try
+                (DirectoryStream<Path> stream = Files.newDirectoryStream(root)) {
+            for (Path path : stream) {
+                if (!Files.isDirectory(path)) {
+                    if (path.endsWith(ext)) {
+                        list.add(path.getFileName().toString());
+                    } else {
+                        System.out.println(" File not found. ");
+                    }
+                }
             }
         }
         return list;
